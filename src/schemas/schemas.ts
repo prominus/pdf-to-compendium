6a9a2e78c19a4adf9c18701921010a45a908c51d
prominus <cody.leclair.us@protonmail.com>
2024-01-10 19:05:35 -0500

    Add 'Fly Free or Die' pawn layout

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -3,9 +3,11 @@ import { iStarfinderDoc } from "@mappings";
 import alien_archive_1 from "./sfrpg/alien_archive_1.json";
 import alien_archive_2 from "./sfrpg/alien_archive_2.json";
 import alien_archive_3 from "./sfrpg/alien_archive_3.json";
+import fly_free_or_die_pawns from "./sfrpg/fly_free_or_die_pawns.json";
 
 export const StarfinderList: iStarfinderDoc[] = [
     alien_archive_1,
     alien_archive_2,
     alien_archive_3,
+    fly_free_or_die_pawns
 ];
--- src/workers/image_saver.ts
+++ src/workers/image_saver.ts
@@ -31,10 +31,14 @@ export async function generateJNodeImages(prefix: string, extMods: string[], dic
             // Create the file
             const file = `${prefix}/${key.toLowerCase().replace(re1, '_').replace(re2, '').replace(re3, '-')}.${x}.png`;
             // Special case for token
-            if (x === 'token' && value.icon !== undefined) {
-                const image = images.find(y => y.name === value.icon);
-                // TODO - put it in a circle banner
-                await saveToken(image, file);
+            if (x === 'token') {
+                if (value.icon == undefined) {
+                    const image = images.find(y => y.name === value.orig);
+                    await saveToken(image, file);
+                } else {
+                    const image = images.find(y => y.name === value.icon);
+                    await saveToken(image, file);
+                }
             } else {
                 const image = images.find(y => y.name === value.orig);
                 await saveImage(image, file);
@@ -56,52 +60,31 @@ async function saveImage(image: ImageMap | undefined, file: string) {
     }
 }
 
-const tokenizeImage = Buffer.from(
-    '<svg><rect x="0" y="0" width="256" height="256" rx="256" ry="256"/></svg>'
-);
+// const tokenizeImage = Buffer.from(
+//     '<svg><rect x="0" y="0" width="256" height="256" rx="256" ry="256"/></svg>'
+// );
 
 async function saveToken(image: ImageMap | undefined, file: string) {
     if (image != undefined) {
         const width = image.width;
         const height = image.height;
         const channels = image.channels();
+        const roundedImage = Buffer.from(
+            `<svg><rect x="0" y="0" width="${width}" height="${height}" rx="50" ry="50"/></svg>`
+        );
         await sharp(image.data, {
             raw: { width, height, channels }
         })
-            .resize(256, 256)
             .flatten({
-                background: '#ffffff'
+                background: '#9bb1c2'
             })
             .composite([
                 {
-                    input: tokenizeImage,
+                    input: roundedImage,
                     blend: 'dest-in'
                 }
             ])
             .toFile(file);
         console.log(`Image saved ${file}`);
     }
-}
-
-async function tokenizeOriginalImage(image: ImageMap | undefined, file: string) {
-    if (image != undefined) {
-        const width = image.width;
-        const height = image.height;
-        const channels = image.channels();
-        await sharp(image.data, {
-            raw: { width, height, channels }
-        })
-            .resize(256, 256)
-            .flatten({
-                background: '#ffffff'
-            })
-            .composite([
-                {
-                    input: tokenizeImage,
-                    blend: 'dest-in'
-                }
-            ])
-            .toFile(file);
-    }
-    console.log(`Image saved ${file}`);
 }
